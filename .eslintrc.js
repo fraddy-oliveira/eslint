a8acc030a87df794970d0fec02d19e6b01c125ec
Fraddy Oliveira <fraddy.dev@gmail.com>
2021-09-28 16:00:01 +0530

    Update eslint env and parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es2021: true,
+    es2017: true,
   },
   extends: [
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
